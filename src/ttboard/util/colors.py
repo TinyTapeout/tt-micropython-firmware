b14ca4dfe0d4816fba50a7b13b35eed706f83d56
Pat Deegan <pat.devel@psychogenic.com>
2024-11-23 16:49:36 -0500

    color management enhancements

--- src/ttboard/util/colors.py
+++ src/ttboard/util/colors.py
@@ -24,6 +24,23 @@ def color(s, color, bright = True):
     if not Enable:
         return s 
     
+    return color_start_code(color, bright) + s + color_end_code()
+
+def color_start(color, bright:bool = True):
+    print(color_start_code(color, bright), end='')
+def color_end():
+    print(color_end_code())
+def color_start_code(color, bright:bool = True):
+    if not Enable:
+        return ''
+    
     code= str(COLORS.index(color))
     suffix = ";1" if bright else ""
-    return f"\033[3{code}{suffix}m{s}\033[0m"
+    return f"\033[3{code}{suffix}m"
+
+def color_end_code():
+    if not Enable:
+        return ''
+    
+    return '\033[0m'
+    
