40c17073cf8fc88cde2ac434b00506cb2ad596e2
Pat Deegan <pat.devel@psychogenic.com>
2024-12-09 11:36:35 -0500

    remove junk files

--- src/yo.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from ttboard.project_mux import *
-d = DesignIndex(None, 'ho.json')
-f = open('/tmp/pik', 'wb')
-pickle.dump(d, f)
-f.close()
-f = open('/tmp/pik', 'rb')
-# >>> a = pickle.load(f)
-
