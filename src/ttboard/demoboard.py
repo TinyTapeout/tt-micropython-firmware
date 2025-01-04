eee4e08dddeacb91c1e6828b3fde60dc5cf6a8ef
Uri Shaked <uri@urishaked.com>
2025-01-04 20:41:32 +0200

    Workaround for TT07 CB error: CTRL_SEL_nRST/CTRL_SEL_INC pins swapped

--- src/ttboard/demoboard.py
+++ src/ttboard/demoboard.py
@@ -18,6 +18,7 @@ import ttboard
 import ttboard.util.time as time
 from ttboard.globals import Globals
 from ttboard.mode import RPMode
+from ttboard.pins.gpio_map import GPIOMapTT06
 from ttboard.pins.pins import Pins
 from ttboard.project_mux import Design
 from ttboard.config.user_config import UserConfig
@@ -139,6 +140,10 @@ class DemoBoard:
             setattr(self, p, getattr(self.pins, p))
             
         self.shuttle = Globals.project_mux(self.user_config.force_shuttle)
+        if self.shuttle.run == 'tt07':
+            pins_mode = self.pins.mode
+            GPIOMapTT06.tt07_cb_fix = True
+            self.pins.mode = pins_mode # force re-init of pins to apply new pin map
         
         # config
         self.apply_configs = apply_user_config
